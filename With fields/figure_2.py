74a2af766e89da2d0c57d20999bdf73f5743065b
AVELL-Breniak <gubiofisika@gmail.com>
2025-12-17 09:31:34 -0300

    add data for  new coluns in parameter datar. add script for for  new coluns in parameter datar. add figure for visualization the statistic

--- With fields/figure_2.py
+++ With fields/figure_2.py
@@ -4,7 +4,7 @@ import numpy  as np
 import matplotlib.pyplot as plt
 
 # Parâmetros do range de N_c
-Nc_values       = [0,0.02,0.04,0.06,0.08,0.1]
+std_values       = [0,0.02,0.04,0.06,0.08,0.1]
 parametros_gamma= ['gamma1', 'gamma2', 'gamma3', 'gamma4']
 parametros_J    = ['JXX', 'JXY', 'JXZ', 'JYX', 'JYY', 'JYZ', 'JZX', 'JZY', 'JZZ', 'JIX', 'JIY', 'JIZ', 'JXI', 'JYI', 'JZI']
 
@@ -18,12 +18,12 @@ erro_js_Nc_min      = []
 erro_gamma_Nc_max   = []
 erro_js_Nc_max      = []
 
-for N in Nc_values:
+for std in std_values:
     erro_gamma  = []
     erro_js     = []
     for i in range(5, 101):
         
-        caminho     = f"C:/Users/Gubio/CODESACE/Pinn inverse for opem quantum system/With fields/data/parametro_withfields_N50_seed{i}_std{N}.csv"
+        caminho     = f"/home/gubio/CODESPACE/Pinn-inverse-for-opem-quantum-system/With fields/data/parametro_withfields_N50_seed{i}_std{std}.csv"
         try:
             
             df          = pd.read_csv(caminho, index_col=0)
@@ -36,7 +36,7 @@ for N in Nc_values:
             erro_js.append(np.mean(erro_abs[parametros_J].to_numpy()))
 
         except FileNotFoundError:
-            print(f"parametro_withfields_N{N}_seed{i}_std0.csv")
+            print(f"parametro_withfields_N50_seed{i}_std{std}.csv")
             continue
 
     # Média, mínimo e máximo dos erros para cada parâmetro
@@ -51,25 +51,24 @@ for N in Nc_values:
 # Plot
 plt.rcParams.update({'font.size': 24})
 plt.figure(figsize=(8,6))
-Nc_arr = np.array(Nc_values)
+std_arr = np.array(std_values)
 
 # Plot Js médio com traço mais espesso
 yerr_J = [np.array(erro_js_Nc) - np.array(erro_js_Nc_min), np.array(erro_js_Nc_max) - np.array(erro_js_Nc)]
-plt.errorbar(Nc_arr, erro_js_Nc, yerr=yerr_J, fmt='ks', capsize=8, label=r'$J_{médio}$', linewidth=2.2)
-
+plt.errorbar(std_arr, erro_js_Nc, yerr=yerr_J, fmt='ks', capsize=8, label=r'$J_{médio}$', linewidth=2.2)
 erro_gamma_Nc       = np.array(erro_gamma_Nc)
 erro_gamma_Nc_min   = np.array(erro_gamma_Nc_min)
 erro_gamma_Nc_max   = np.array(erro_gamma_Nc_max)
 
 yerr_g = [erro_gamma_Nc - erro_gamma_Nc_min , erro_gamma_Nc_max - erro_gamma_Nc]
-plt.errorbar(Nc_arr, erro_gamma_Nc, yerr=yerr_g, fmt='o', color='r', capsize=8, linewidth=2.2,label=r'$\gamma_{médio}$') 
+plt.errorbar(std_arr, erro_gamma_Nc, yerr=yerr_g, fmt='o', color='r', capsize=8, linewidth=2.2,label=r'$\gamma_{médio}$') 
 
 
 plt.yscale('log')
 plt.ylabel('MSE')
 plt.xlabel('Desvio padrão')   
 
-plt.xticks(Nc_values)
+plt.xticks(std_values)
 plt.tick_params(axis='both', which='both', direction='in', length=4, width=1.5)
 plt.legend(loc='lower right', ncol=2, fontsize=20, framealpha=0.8, handletextpad=0.01, columnspacing=0.1)
 
